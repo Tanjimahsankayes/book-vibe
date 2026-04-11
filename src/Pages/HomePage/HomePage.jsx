dafeb606c5456532ff8ca42664d3523f9044f5c8
tanjimahsankayes@gmail.com <tanjimahsankayes@gmail.com>
2026-04-11 14:23:14 +0600

    banner responsive

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import BannerImg from '../../assets/Banner-book.png'
 
 const HomePage = () => {
     return (
       <div>
         <div className="hero bg-base-200 min-h-[70vh] rounded-2xl my-8 container mx-auto ">
           <div className="hero-content flex-col lg:flex-row-reverse w-full justify-between px-20">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-              className="max-w-sm rounded-lg shadow-2xl"
-            />
+            <img className="w-70 h-70" src={BannerImg} />
             <div>
-              <h1 className="text-5xl font-bold">Book to freshen up <br /> your bookself</h1>
-              <button className="btn btn-success mt-8 "> View the List </button>
+              <h1 className=" text-3xl text-center md:text-left md:text-5xl font-bold">
+                Book to freshen up <br /> your bookself
+              </h1>
+              <div className='flex justify-center md:justify-start ' >
+                <button className="btn btn-success mt-8 text-white ">
+                  View the List
+              </div>
             </div>
           </div>
         </div>
